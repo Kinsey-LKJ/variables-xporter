2446c2817889810f24f1c3f890eebeacb3c0cc59
Kinsey-LKJ <kinsey@lkj.design>
2024-12-03 18:30:07 +0800

    feat: :sparkles: 初始化文档目录

---
+++ app/_meta.js
@@ -0,0 +1,20 @@
+const meta = {
+  index: {
+    display: 'hidden'
+  },
+  docs: {
+    type: 'page',
+    title: 'Documentation',
+  },
+  example: {
+    type: 'page',
+    title: 'Example'
+  },
+  blog: {
+    type: 'page',
+    title: 'Blog'
+  },
+
+}
+
+export default meta
