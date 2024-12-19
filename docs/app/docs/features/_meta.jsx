10dbceb1ce2139e38964ce58c1060eeb79e02654
Kinsey-LKJ <kinsey@lkj.design>
2024-12-19 18:24:04 +0800

    fix(docs): :bug: 修复 features 的子页面不在左侧边栏展示的问题

--- docs/app/docs/features/_meta.jsx
+++ docs/app/docs/features/_meta.jsx
@@ -1,27 +1,22 @@
 const meta = {
   'variable-references': {
     title: '变量引用',
-    type: 'page'
   },
   'unit-conversion': {
     title: '单位转换',
-    type: 'page'
   },
   'multi-mode': {
     title: '多模式支持',
-    type: 'page'
   },
   'ignore-tailwind-palette': {
     title: '忽略 Tailwind 调色板',
-    type: 'page'
   },
   'merge-typography-styles': {
     title: '合并排版样式',
-    type: 'page'
   },
   'css-media-query-mode': {
     title: 'CSS 媒体查询模式',
-    type: 'page'
+    
   }
 };
 
