8d20da0833d83d8df7bb6988721c6eb6be49f698
Kinsey-LKJ <kinsey@lkj.design>
2025-07-07 11:02:20 +0200

    feat(docs): :sparkles: 优化文档的结构和 shadcn/ui 相关的内容

--- docs/content/fr/docs/_meta.js
+++ docs/content/fr/docs/_meta.js
@@ -1,10 +1,10 @@
 const meta = {
   index: "",
-  "single-outlet-principle": "Principe de Sortie Unique",
-  "shadcn-ui": "Intégration shadcn/ui",
+  "shadcn-ui": "Export de Thème shadcn/ui",
   'export-modes':"Modes d'Exportation",
   "organizing-your-variables": 'Organiser Vos Figma Variables',
   features: "Fonctionnalités",
+  "single-outlet-principle": "Principe de Sortie Unique",
 };
 
 export default meta;
